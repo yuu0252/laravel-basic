c2d37a1e59cc7654d2263f3ab6ac96d20f7e8a16
yuu0252 <yutanmaron@gmail.com>
2023-12-31 17:35:20 +0900

    error handle

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 
 class HelloController extends Controller
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel基礎</title>
+</head>
+
+<body>
+    <p>ページが見つかりません。</p>
+</body>
+
+</html>
