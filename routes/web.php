d00018806f3489e496a40d74dd3c14f4a33a6d60
yuu0252 <yutanmaron@gmail.com>
2024-01-07 07:44:44 +0900

    laravel breeze installed

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,7 @@
 <?php
 
-use App\Http\Controllers\CookieController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HelloController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\RequestController;
-use App\Http\Controllers\ResponseController;
-use App\Http\Controllers\SessionController;
-use App\Http\Controllers\SignInController;
-use App\Http\Controllers\VendorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,44 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', [HelloController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/products/create', [ProductController::class, 'create']);
-
-Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
-
-Route::get('/products/{id}', [ProductController::class, 'show']);
-
-Route::get('/vendors/create', [VendorController::class, 'create']);
-
-Route::post('/vendors/store', [VendorController::class, 'store'])->name('vendors.store');
-
-Route::get('/vendors/{id}', [VendorController::class, 'show']);
-
-Route::get('/requests/create', [RequestController::class, 'create']);
-
-Route::post('/requests/confirm', [RequestController::class, 'confirm'])->name('requests.confirm');
-
-Route::get('/responses', [ResponseController::class, 'index']);
-
-Route::get('/sign-in', [SignInController::class, 'create']);
-
-Route::post('/sign-in', [SignInController::class, 'store'])->name('sign-in.store');
-
-Route::get('/cookies', [CookieController::class, 'index']);
-
-Route::get('/cookies/create', [CookieController::class, 'create'])->name('cookies.create');
-
-Route::post('/cookies/store', [CookieController::class, 'store'])->name('cookies.store');
-
-Route::delete('/cookies/destroy', [CookieController::class, 'destroy'])->name('cookies.destroy');
-
-Route::get('/sessions', [SessionController::class, 'index']);
-
-Route::get('/sessions/create', [SessionController::class, 'create'])->name('sessions.create');
-
-Route::post('/sessions/store', [SessionController::class, 'store'])->name('sessions.store');
-
-Route::delete('/sessions/destroy', [SessionController::class, 'destroy'])->name('sessions.destroy');
+require __DIR__.'/auth.php';
